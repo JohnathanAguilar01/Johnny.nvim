2469835604062311b1e446b3a19beea2bdc6ad84
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-09-26 00:28:59 -0700

    fixed issues with spellcheck not higligting

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,23 @@
 return {
 	"folke/tokyonight.nvim",
 	priority = 9000,
-	config = function()
-		require("tokyonight").setup({
+	opts = {
+		on_highlights = function(hl, c)
+			hl.SpellBad = { fg = c.red, undercurl = true }
+			hl.SpellCap = { fg = c.red, undercurl = true }
+			hl.SpellLocal = { fg = c.red, undercurl = true }
+			hl.SpellRare = { fg = c.red, undercurl = true }
+		end,
+	},
+	config = function(_, opts)
+		require("tokyonight").setup(vim.tbl_deep_extend("force", {
 			-- style = "night",
 			transparent = true,
 			styles = {
 				sidebars = "transparent",
 				floats = "transparent",
 			},
-		})
+		}, opts))
 
 		vim.cmd("colorscheme tokyonight")
 	end,
